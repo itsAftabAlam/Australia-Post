bbc0a3b594bdde66ba370ec93adeaf7eaff91977
Aftab Alam <88653530+itsAftabAlam@users.noreply.github.com>
2024-03-24 00:08:44 +0530

    added shipment tracking feature

--- src/main/java/AustraliaPost.java
+++ src/main/java/AustraliaPost.java
@@ -3,6 +3,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Base64;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -41,6 +43,38 @@ public class AustraliaPost {
                 System.out.println("Error: " + response.statusCode() + " - " + response.body());
             }
         } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void track(String username, String password, String accountNumber, String trackingIds) throws Exception {
+        // Construct authentication header
+        String auth = username + ":" + password;
+        String encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes());
+
+        // Construct request URI
+        String url = "https://digitalapi.auspost.com.au/shipping/v1/track?tracking_ids=" + trackingIds;
+
+        // Create HTTP client and request
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("authorization", "Basic " + encodedAuth)
+                    .header("account-number", accountNumber)
+                    .GET()
+                    .build();
+
+        // Send request and handle response
+        try{
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200) {
+                System.out.println(response.body());
+            }
+            else {
+                throw new Exception("Error: " + response.statusCode() + " - " + response.body());
+            }
+        }
+        catch (Exception e){
             e.printStackTrace();
         }
     }
